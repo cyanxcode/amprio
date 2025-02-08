0686e739b40737fa3652f3a445123e305a601d0e
Cyan <codinginstant@gmail.com>
2025-02-09 02:13:02 +0530

    added link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Quote from "@/components/Quote";
 import Reviews from "@/components/Reviews";
 import FAQs from "../components/FAQs";
 import Hero from "@/components/Hero";
+import Link from "next/link";
 
 export default async function Main() {
   return (
@@ -14,13 +15,13 @@ export default async function Main() {
         </div>
         <DisplayByTag tag={"New Arrival"} />
       </div>
-      <div className="min-h-96">
+      <Link className="min-h-96" href="/chandeliers">
         <img
           src="https://cdn.shopify.com/s/files/1/0677/0085/0871/files/9.png?v=1739046379"
           className="w-full my-20 h-full min-h-96 object-cover"
           alt=""
         />
-      </div>
+      </Link>
       <div className=" mt-10">
         <h2 className="w-full text-center text-3xl md:text-6xl font-italiana">
           Best Sellers
@@ -34,11 +35,13 @@ export default async function Main() {
         </h2>
       </div>
       <Reviews />
-      <img
-        src="https://cdn.shopify.com/s/files/1/0677/0085/0871/files/5.png?v=1739046379"
-        className="w-full my-20 hidden md:block"
-        alt=""
-      />
+      <Link href="/">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0677/0085/0871/files/5.png?v=1739046379"
+          className="w-full my-20 hidden md:block"
+          alt=""
+        />
+      </Link>
       <FAQs />
     </>
   );
