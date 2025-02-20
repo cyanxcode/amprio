e626c38811457174ba89abf05ad2a5e35dea213d
Cyan <codinginstant@gmail.com>
2025-02-20 12:32:03 +0530

    minor

--- src/components/HeroX.tsx
+++ src/components/HeroX.tsx
@@ -19,7 +19,7 @@ export default function HeroX({ category }: Props) {
         <div className="relative z-10">
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
             <div className="max-w-2xl text-center mx-auto">
-              <p className="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l text-[#023235]">
+              <p className="inline-block text-sm font-medium bg-clip-text text-[#023235]">
                 Welcome to Amprio
               </p>
 
